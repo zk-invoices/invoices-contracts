fd19f9f2eacfc10dd415ebf9e269bc2e1410a5aa
Shashank <kriss1897@users.noreply.github.com>
2023-12-17 23:49:48 +0530

    [In Progress] Try having Invoices zkApp instances linked per token

--- src/experiments/customTokenAccounts.ts
+++ src/experiments/customTokenAccounts.ts
@@ -8,12 +8,193 @@ import {
   VerificationKey,
   Permissions,
   method,
-  state,
-  State,
   Cache,
   Mina,
-  PrivateKey
+  PrivateKey,
+  MerkleTree,
+  UInt32,
+  MerkleWitness,
+  Struct,
+  Poseidon,
+  state,
+  State,
+  Reducer,
+  Provable
 } from 'o1js';
+
+export class InvoicesWitness extends MerkleWitness(32) {}
+
+export class Invoice extends Struct({
+  from: PublicKey,
+  to: PublicKey,
+  amount: UInt32,
+  settled: Bool,
+  metadataHash: Field,
+}) {
+  hash(): Field {
+    return Poseidon.hash(Invoice.toFields(this));
+  }
+
+  settle() {
+    return new Invoice({
+      from: this.from,
+      to: this.to,
+      amount: this.amount,
+      metadataHash: this.metadataHash,
+      settled: Bool(true),
+    });
+  }
+}
+// we now need "wrap" the Merkle tree around our off-chain storage
+// we initialize a new Merkle Tree with height 8
+const initialRoot = new MerkleTree(32).getRoot();
+export class InvoiceOperation extends Struct({
+  type: Field,
+  invoice: Invoice,
+  witness: InvoicesWitness,
+}) {
+  isCreate() {
+    return this.type.equals(Field(0));
+  }
+
+  isSettle() {
+    return this.type.equals(Field(1));
+  }
+
+  static create(invoice: Invoice, witness: InvoicesWitness) {
+    return new InvoiceOperation({
+      type: Field(0),
+      invoice,
+      witness,
+    });
+  }
+
+  static settle(invoice: Invoice, witness: InvoicesWitness) {
+    return new InvoiceOperation({
+      type: Field(1),
+      invoice,
+      witness,
+    });
+  }
+}
+
+export class Invoices extends SmartContract {
+  @state(Field) commitment = State<Field>();
+  @state(Field) accumulated = State<Field>();
+
+  reducer = Reducer({ actionType: InvoiceOperation });
+
+  @method init() {
+    super.init();
+    this.accumulated.set(Reducer.initialActionState);
+    this.commitment.set(initialRoot);
+  }
+
+  @method
+  createInvoice(invoice: Invoice, path: InvoicesWitness) {
+    let commit = this.commitment.get();
+    this.commitment.assertEquals(commit);
+
+    path
+      .calculateRoot(Field(0))
+      .equals(commit)
+      .assertTrue('The invoice already exists');
+
+    let accumulated = this.accumulated.get();
+    this.accumulated.assertEquals(accumulated);
+
+    let pendingActions = this.reducer.getActions({
+      fromActionState: accumulated,
+    });
+
+    let { state } = this.reducer.reduce(
+      pendingActions,
+      Bool,
+      (state: Bool, action: InvoiceOperation) => {
+        return Bool(
+          action.isCreate().and(action.invoice.hash().equals(invoice.hash()))
+        ).or(state);
+      },
+      { state: Bool(false), actionState: accumulated }
+    );
+
+    state.assertFalse('The invoice already exists');
+
+    this.reducer.dispatch(InvoiceOperation.create(invoice, path));
+  }
+
+  @method
+  settleInvoice(invoice: Invoice, path: InvoicesWitness) {
+    let commit = this.commitment.get();
+    this.commitment.assertEquals(commit);
+
+    let accumulated = this.accumulated.get();
+    this.accumulated.assertEquals(accumulated);
+
+    invoice.settled.assertFalse('Invoice is already settled');
+    let isCreateCommitted = path.calculateRoot(invoice.hash()).equals(commit);
+
+    let { state: isCreatePending } = this.reducer.reduce(
+      this.reducer.getActions({ fromActionState: accumulated }),
+      Bool,
+      (state: Bool, action: InvoiceOperation) => {
+        return Bool(
+          action.isCreate().and(action.invoice.hash().equals(invoice.hash()))
+        ).or(state);
+      },
+      { state: Bool(false), actionState: accumulated }
+    );
+
+    isCreateCommitted.or(isCreatePending).assertTrue('Invoice not created yet');
+
+    let { state: isSettlePending } = this.reducer.reduce(
+      this.reducer.getActions({ fromActionState: accumulated }),
+      Bool,
+      (state: Bool, action: InvoiceOperation) => {
+        return Bool(
+          action.isSettle().and(action.invoice.hash().equals(invoice.hash()))
+        ).or(state);
+      },
+      { state: Bool(false), actionState: accumulated }
+    );
+
+    isSettlePending.assertFalse('Invoice settlement already in queue');
+
+    this.reducer.dispatch(InvoiceOperation.settle(invoice, path));
+  }
+
+  @method
+  commit() {
+    let accumulated = this.accumulated.get();
+    this.accumulated.assertEquals(accumulated);
+
+    let commitment = this.commitment.get();
+    this.commitment.assertEquals(commitment);
+
+    let pendingActions = this.reducer.getActions({
+      fromActionState: accumulated,
+    });
+
+    let { state: newCommitment, actionState: newAccumulated } =
+      this.reducer.reduce(
+        pendingActions,
+        Field,
+        (state: Field, action: InvoiceOperation) => {
+          return action.witness.calculateRoot(
+            Provable.if(
+              action.isCreate(),
+              action.invoice.hash(),
+              action.invoice.settle().hash()
+            )
+          );
+        },
+        { state: commitment, actionState: accumulated }
+      );
+
+    this.commitment.set(newCommitment);
+    this.accumulated.set(newAccumulated);
+  }
+}
 
 const doProofs = true;
 
@@ -46,19 +227,19 @@ export class Token extends SmartContract {
     };
   }
 
-  @method update(value: Field, address: PublicKey) {
-    const zkAppTokenAccount = new TokenAccount(address, this.token.id);
-    zkAppTokenAccount.update(value);
+  @method createInvoice(address: PublicKey, invoice: Invoice, path: InvoicesWitness) {
+    const zkAppTokenAccount = new Invoices(address, this.token.id);
+    zkAppTokenAccount.createInvoice(invoice, path);
   }
-}
 
-export class TokenAccount extends SmartContract {
-  @state(Field) value = State<Field>();
+  @method settleInvoice(address: PublicKey, invoice: Invoice, path: InvoicesWitness) {
+    const zkAppTokenAccount = new Invoices(address, this.token.id);
+    zkAppTokenAccount.settleInvoice(invoice, path);
+  }
 
-  @method update(value: Field) {
-    const oldValue = this.value.getAndAssertEquals();
-    oldValue.assertEquals(value.sub(Field(1)));
-    this.value.set(value);
+  @method commit(address: PublicKey) {
+    const zkAppTokenAccount = new Invoices(address, this.token.id);
+    zkAppTokenAccount.commit();
   }
 }
 
@@ -77,6 +258,8 @@ async function run() {
   let zkappKey = PrivateKey.random();
   let zkappAddress = zkappKey.toPublicKey();
 
+  const Tree = new MerkleTree(32);
+
   // now that we got our accounts set up, we need the commitment to deploy our contract!
   
   let tokensApp = new Token(zkappAddress);
@@ -84,7 +267,7 @@ async function run() {
   console.time();
   const cache: Cache = Cache.FileSystem("./localcache");
   
-  const { verificationKey } = (await TokenAccount.compile());
+  const { verificationKey } = (await Invoices.compile());
 
   await Token.compile({ cache });
   console.timeEnd();
@@ -119,8 +302,18 @@ async function run() {
 
   async function update() {
     console.log('updating');
+    const invoice = new Invoice({
+      from: Local.testAccounts[1].publicKey,
+      to: Local.testAccounts[1].publicKey,
+      amount: UInt32.from(1),
+      settled: Bool(false),
+      metadataHash: Field(0),
+    });
+    const w = Tree.getWitness(0n);
+    let witness = new InvoicesWitness(w);
+
     let tx = await Mina.transaction(feePayer, () => {
-      tokensApp.update(Field(1), userPublicKey);
+      tokensApp.createInvoice(userPublicKey, invoice, witness);
     });
     await tx.prove();
     await tx.sign([feePayerKey, userPrivateKey]).send();
